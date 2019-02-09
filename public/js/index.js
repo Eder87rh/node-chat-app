f6b455c7513a5bac8f58f9b64a3c4b34ee813d5e
Eder Ramirez <documentador1@telecomo.com.mx>
2019-02-09 10:37:51 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Eder',
-        text: 'Hey this is Eder.'
-    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
-    socket.emit('newMessage', {
-        from: 'Naye',
-        text: 'Hello!',
-        createdAt: 123
-    });
-
     socket.on('createMessage', message => {
-		console.log('TCL: message', message)
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
