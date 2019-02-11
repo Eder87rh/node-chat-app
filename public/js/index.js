98c4968f076d7678d98076a1c3025f78aff5c019
Eder Ramirez <documentador1@telecomo.com.mx>
2019-02-11 11:08:33 -0600

    message form and JQuery

--- public/js/index.js
+++ public/js/index.js
@@ -10,5 +10,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('New message', message);
+    var li = document.createElement("LI");
+    li.innerText = `${message.from}: ${message.text}`;
+
+    document.getElementById('messages').appendChild(li);
 });
 
+var form = document.getElementById('message-form').addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: document.getElementById("message").value
+    }, function () {
+
+    });
+});
--- server/server.js
+++ server/server.js
@@ -20,20 +20,10 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-
-
-    socket.on('createMessage', message => {
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        });
-
-        /* socket.broadcast.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        }); */
+    socket.on('createMessage', (message, callback) => {
+		console.log('TCL: message', message)
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
     });
 
     socket.on('disconnect', () => {
